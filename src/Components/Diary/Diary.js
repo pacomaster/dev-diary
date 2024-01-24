73aa88318bc7e3bf9bafb0ccb79e8b86060f5ada
pacomaster <fco_amezcua@hotmail.com>
2024-01-23 23:48:26 -0600

    dynamic entries

--- src/Components/Diary/Diary.js
+++ src/Components/Diary/Diary.js
@@ -2,13 +2,44 @@ import { useState } from 'react';
 
 export default function Diary(){
 
-    const [date, setDate] = useState('');
+    var today = new Date();
+    var virtualMonth = today.getMonth() + 1; // This is to debug month
+    var month = virtualMonth < 10 ? '0' + virtualMonth : virtualMonth;
+    var virtualDay = today.getDate(); // This is to debug day
+    var day = virtualDay < 10 ? '0' + virtualDay : virtualDay;
+    var dateStr = today.getFullYear() + '-' + month + '-' + day;
+
+    const [date, setDate] = useState(dateStr);
     const [content, setContent] = useState('');
     const [entries, setEntries] = useState([]);
 
-    function addEntry(){
-        alert(`You said ${date} to ${content}`);
-        setEntries([...entries, content]);
+    function removeEntry(checkDate){
+            //var goodEntries = entries.filter((entry) => checkDate !== entry["date"]);
+            //console.log("goodEntries");
+            //console.log(goodEntries);
+            setEntries(entries.filter((entry) => checkDate !== entry["date"]));
+        }
+
+    function findEntry(checkDate){
+        var value = entries.filter((entry) => checkDate === entry["date"]);
+        return value
+    }
+
+    function addEntry(e){
+        removeEntry(date);
+        console.log("entries");
+        console.log(entries);
+
+        var item = {"date" : date, "content": content};
+        setEntries([...entries, item]);
+    }
+
+    function changeDate(e){
+        var value = findEntry(e.target.value);
+        if(value.length > 0){
+            setContent(value[0]["content"]);
+        }
+        setDate(e.target.value);
     }
 
     return (
@@ -16,7 +47,7 @@ export default function Diary(){
             <header>
                 <button id="read">Read</button>
                 <button id="write">Write</button>
-                <input type="date" id="date" value={date} onChange={e => setDate(e.target.value)} />
+                <input type="date" id="date" value={date} onChange={changeDate} />
             </header>
             <body>
                 <textarea name="content" value={content} onChange={e => setContent(e.target.value)} />
@@ -24,7 +55,10 @@ export default function Diary(){
                 <div>ENTRIES:</div>
                 {entries.map((entry) => {
                     return(
-                        <div>{entry}</div>
+                        <div>
+                            <div>{entry["date"]}</div>
+                            <div>{entry["content"]}</div>
+                        </div>
                     );
                 })}
             </body>
